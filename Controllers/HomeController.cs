34da7afc1f81914939164829692cc3f5552f8ffd
Domantas Pelaitis <domantas.pelaitis@gmail.com>
2018-04-26 10:26:28 +0300

    Adding new project with identity and connection to db

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FiveDevsShop.Models;
+using FiveDevsShop.Data;
+
+namespace FiveDevsShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            //var categories = db.Category.ToList();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
