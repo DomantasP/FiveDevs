b0664b0800f8b8d147fe971c4b6c0248a1202c7c
Domantas Jadenkus <djadenkus@gmail.com>
2018-05-26 13:39:49 +0300

    basic list view

---
+++ Models/ViewModels/ProductPreviewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiveDevsShop.Models
+{
+    public class ProductPreviewModel
+    {
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Discount { get; set; }
+
+        public string MainImageUrl { get; set; }
+    }
+}
